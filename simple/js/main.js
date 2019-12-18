d0c1e16effef31cfb6c6b9a9e5984f8ef6425202
Vladislav Shevchenko <shev.vlad1991@gmail.com>
2019-12-18 18:34:09 +0200

    add js for rotate

--- simple/js/main.js
+++ simple/js/main.js
@@ -1,22 +1,8 @@
-// let front = document.querySelectorAll('.card__front');
-// let back = document.querySelectorAll('.card__back');
-let card = document.querySelectorAll('.card__back, .card__front');
+let card = document.querySelectorAll('.card');
 
 
 card.forEach(function (c) {
-    this.addEventListener('click', function () {
+    c.addEventListener('click', function () {
         c.classList.toggle('active');
     });
 });
-
-// front.forEach(function (f) {
-//     f.addEventListener('click', function () {
-//         f.classList.toggle('active');
-//     });
-// });
-
-// back.forEach(function (b) {
-//     b.addEventListener('click', function () {
-//         b.classList.toggle('active');
-//     });
-// });
